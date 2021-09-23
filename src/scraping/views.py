ed6d6f4c9022cfdff0211c854fcf66209101316b
nitkevs <qdfqw>
2021-09-23 17:58:57 +0300

    commit

--- src/scraping/views.py
+++ src/scraping/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Vacancy
+
+def home_view(request):
+    city = request.GET.get('city')
+    language = request.GET.get('language')
+    _filter = {}
+    if city or language:
+        if city:
+            _filter['city__name'] = city
+        if language:
+            _filter['language__name'] = language
+    qs = Vacancy.objects.filter(**_filter)
+    return render(request, 'scraping/home.html', {'object_list': qs})
